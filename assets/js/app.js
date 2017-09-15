d2f49703da087ed0f391b409957ee0d96bfc5af0
Ian McGonigle <ian.is.mcgonigle@gmail.com>
2017-09-14 20:36:32 -0600

    Generate pixels through JavaScript.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,36 @@
-let pixels = document.getElementsByClassName('pixel');
 let colorInput = document.getElementById('color-input');
+let canvas = document.getElementById('canvas');
 
 let selectedColor = colorInput.value;
 let isMouseDown = false;
+
+const pixelHeight = 10;
+const pixelWidth = 10;
+
+let numberOfPixelsAcross = (canvas.offsetWidth / pixelWidth).toPrecision(5);
+let numberOfPixelsTall = (canvas.offsetHeight /pixelHeight).toPrecision(5);
+
+let numberOfPixelsToAddToCanvas = numberOfPixelsAcross * numberOfPixelsTall;
+
+let pixels = [];
+
+while (numberOfPixelsToAddToCanvas > 0) {
+  let pixel = document.createElement('div');
+  pixel.style.width = `${pixelWidth}px`;
+  pixel.style.height = `${pixelHeight}px`;
+  pixel.className += 'pixel';
+
+  pixels.push(pixel);
+  canvas.appendChild(pixel);
+
+  numberOfPixelsToAddToCanvas--;
+}
 
 for (let i = 0; i < pixels.length; i++) {
   let pixel = pixels[i];
   
   pixel.addEventListener('mouseover', function(event) {
-    if(isMouseDown) {
+    if (isMouseDown) {
       this.style.background = selectedColor;
     }
   });
