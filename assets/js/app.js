1930743744813304cdcbee6ada62732bd6a9a17e
Ian McGonigle <ian.is.mcgonigle@gmail.com>
2017-09-14 23:42:59 -0600

    Add some styles and change the event listener.

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,27 +2,23 @@ let colorInput = document.getElementById('color-input');
 let canvas = document.getElementById('canvas');
 
 let selectedColor = colorInput.value;
-let isMouseDown = false;
 
-const pixelHeight = 10;
-const pixelWidth = 10;
+const pixelSize = 10;
 
-let numberOfPixelsAcross = (canvas.offsetWidth / pixelWidth).toPrecision(5);
-let numberOfPixelsTall = (canvas.offsetHeight /pixelHeight).toPrecision(5);
-
+let numberOfPixelsAcross = (canvas.offsetWidth / pixelSize).toPrecision(5);
+let numberOfPixelsTall = (canvas.offsetHeight /pixelSize).toPrecision(5);
 let numberOfPixelsToAddToCanvas = numberOfPixelsAcross * numberOfPixelsTall;
 
 let pixels = [];
 
 while (numberOfPixelsToAddToCanvas > 0) {
   let pixel = document.createElement('div');
-  pixel.style.width = `${pixelWidth}px`;
-  pixel.style.height = `${pixelHeight}px`;
-  pixel.className += 'pixel';
 
+  pixel.style.width = `${pixelSize}px`;
+  pixel.style.height = `${pixelSize}px`;
+  pixel.className += 'pixel';
   pixels.push(pixel);
   canvas.appendChild(pixel);
-
   numberOfPixelsToAddToCanvas--;
 }
 
@@ -30,7 +26,7 @@ for (let i = 0; i < pixels.length; i++) {
   let pixel = pixels[i];
   
   pixel.addEventListener('mouseover', function(event) {
-    if (isMouseDown) {
+    if (event.buttons == 1 || event.buttons == 3) {
       this.style.background = selectedColor;
     }
   });
@@ -39,11 +35,3 @@ for (let i = 0; i < pixels.length; i++) {
 colorInput.addEventListener('input', function(event) {
   selectedColor = this.value;
 });
-
-document.addEventListener('mousedown', function (event) {
-  isMouseDown = true;
-});
-
-document.addEventListener('mouseup', function (event) {
-  isMouseDown = false;
-})
